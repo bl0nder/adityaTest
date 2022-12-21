b335b945504527b867886632b4323a19ff71b68f
bl0nder <goelpalaash@gmail.com>
2022-12-21 22:49:03 +0530

    .

--- proc-read.c
+++ proc-read.c
@@ -12,7 +12,18 @@ module_param(pid, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
 static struct pid* pid_struct;
 static struct task_struct* task_list;
 
-int proc_read() {
+// int proc_read() {
+//     int len = 0;
+//     pid_struct = find_get_pid(pid);
+//     if (pid_struct == NULL) pr_info("Null pid found!!\n");
+//     else {
+//         task_list = pid_task(pid_struct, PIDTYPE_PID);
+//         pr_info("%d\n%d\n%d\n", pid, task_list->cred->uid, task_list->cred->gid);
+//     }
+//     return len;
+// }
+
+int read_init(void) {
     int len = 0;
     pid_struct = find_get_pid(pid);
     if (pid_struct == NULL) pr_info("Null pid found!!\n");
@@ -20,11 +31,6 @@ int proc_read() {
         task_list = pid_task(pid_struct, PIDTYPE_PID);
         pr_info("%d\n%d\n%d\n", pid, task_list->cred->uid, task_list->cred->gid);
     }
-    return len;
-}
-
-int read_init(void) {
-    proc_read();
     return 0;
 }
 
